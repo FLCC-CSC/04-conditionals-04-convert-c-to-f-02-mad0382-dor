d36eba10191902b53c59c92261505fc26265328f
Melvin Dorta <mad0382@gmail.com>
2026-03-14 17:48:22 +0000

    convert

--- convert_C_to_F_02.py
+++ convert_C_to_F_02.py
@@ -1,7 +1,7 @@
 # FILE NAME - convert_C_to_F_02.py
 
-# NAME: 
-# DATE: 
+# NAME: Melvin Dorta
+# DATE: March 14th 2026
 # BRIEF DESCRIPTION:  
 
 
@@ -15,7 +15,28 @@
 
 ########## ENTER YER CODE BELOW THIS LINE ##########
 
+print("===== Temperature Converter =====\n")
+print("  1. Convert from Celsius to Fahrenheit")
+print("  2. Convert from Fahrenheit to Celsius\n")
 
+# Ask the user for the conversion choice
+choice = input("Please choose from the above menu: ")
+
+# Celsius to Fahrenheit conversion
+if choice == "1":
+    temp_c = float(input("Enter a temperature to convert: "))
+    temp_f = temp_c * 9/5 + 32
+    print(f"\n{temp_c} degrees Celsius is {temp_f} degrees Fahrenheit.")
+
+# Fahrenheit to Celsius conversion
+elif choice == "2":
+    temp_f = float(input("Enter a temperature to convert: "))
+    temp_c = (temp_f - 32) * 5/9
+    print(f"\n{temp_f} degrees Fahrenheit is {temp_c} degrees Celsius.")
+
+# Handle invalid choices
+else:
+    print("\nInvalid choice. Please run the program again and select 1 or 2.")
 
 
 
